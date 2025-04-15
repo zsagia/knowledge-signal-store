fbd12f9253ac97a0bf7431ba189ea9c3af7a3d8f
zsagia <zsagia@gmail.com>
2025-04-15 16:55:45 +0200

    Refresh table by store

--- src/app/books/components/book-table/book-table.component.ts
+++ src/app/books/components/book-table/book-table.component.ts
@@ -1,18 +1,18 @@
+import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
-import { Book, books } from '../../models';
 import { BookTableStore } from './book-table.store';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TableModule],
+  imports: [ButtonModule, TableModule],
   providers: [BookTableStore],
   templateUrl: './book-table.component.html',
   selector: 'app-book-table',
   styleUrl: './book-table.component.scss',
 })
 export class BookTableComponent {
-  bookTableStore = inject(BookTableStore);
+  public bookTableStore = inject(BookTableStore);
 }
