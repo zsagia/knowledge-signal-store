14c2f375a41565a9e692c9df60c868354705651e
zsagia <zsagia@gmail.com>
2025-04-16 16:40:04 +0200

    Create listBooks method in books store

--- src/app/books/store/books.store.ts
+++ src/app/books/store/books.store.ts
@@ -1,16 +1,49 @@
-import { signalStore, withHooks, withMethods, withProps } from '@ngrx/signals';
-import { withEntities } from '@ngrx/signals/entities';
+import { inject } from '@angular/core';
+import { tapResponse } from '@ngrx/operators';
+import {
+  patchState,
+  signalStore,
+  withHooks,
+  withMethods,
+  withProps,
+} from '@ngrx/signals';
+import {
+  SelectEntityId,
+  setAllEntities,
+  withEntities,
+} from '@ngrx/signals/entities';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+
+import { debounceTime, pipe, switchMap } from 'rxjs';
 
 import { Book } from '../models';
-import { inject } from '@angular/core';
 import { BookDataService } from '../services';
+
+const selectId: SelectEntityId<Book> = (book) => book.uid;
 
 export const BooksStore = signalStore(
   withEntities<Book>(),
   withProps(() => ({
     bookDataService: inject(BookDataService),
   })),
-  withMethods(({ bookDataService, ...store }) => ({})),
+  withMethods(({ bookDataService, ...store }) => ({
+    listBooks: rxMethod<void>(
+      pipe(
+        debounceTime(3000),
+        switchMap(() => {
+          return bookDataService.listBooks().pipe(
+            tapResponse({
+              next: (books) =>
+                patchState(store, setAllEntities(books, { selectId })),
+              error: (err) => {
+                console.error(err);
+              },
+            })
+          );
+        })
+      )
+    ),
+  })),
   withHooks({
     onInit() {
       console.log('BooksStore initialized');
