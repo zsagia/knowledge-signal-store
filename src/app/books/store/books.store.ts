d137a49f40debed13e85f3e14267b39bfe086bce
zsagia <zsagia@gmail.com>
2025-04-16 18:17:39 +0200

    Use withCustomEntity

--- src/app/books/store/books.store.ts
+++ src/app/books/store/books.store.ts
@@ -1,12 +1,15 @@
-import { inject } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import { tapResponse } from '@ngrx/operators';
 import {
   patchState,
   signalStore,
+  signalStoreFeature,
   type,
+  withComputed,
   withHooks,
   withMethods,
   withProps,
+  withState,
 } from '@ngrx/signals';
 import {
   entityConfig,
@@ -25,18 +28,67 @@ const booksStoreConfig = entityConfig({
   selectId: (book) => book.uid,
 });
 
+export type CustomEntityState = {
+  isLoading: boolean;
+  newEntityButtonEnabled: boolean;
+  selectedBook: Book | null;
+};
+
+export function setNewEntityButtonEnabled(
+  newEntityButtonEnabled: boolean
+): Partial<CustomEntityState> {
+  return { newEntityButtonEnabled };
+}
+
+export function setSelectedBook(
+  selectedBook: Book | null
+): Partial<CustomEntityState> {
+  return { selectedBook };
+}
+
+export function setLoading(isLoading: boolean): Partial<CustomEntityState> {
+  return { isLoading };
+}
+
+export function withCustomEntity() {
+  return signalStoreFeature(
+    withState<CustomEntityState>({
+      isLoading: false,
+      newEntityButtonEnabled: false,
+      selectedBook: null,
+    }),
+    withComputed(({ newEntityButtonEnabled }) => ({
+      isNewEntityButtonEnabled: computed(() => newEntityButtonEnabled()),
+    })),
+    withMethods((store) => {
+      return {
+        setNewEntityButtonEnabled(enabled: boolean): void {
+          patchState(store, setNewEntityButtonEnabled(enabled));
+        },
+        setSelectedBook(selectedBook: Book | null): void {
+          patchState(store, setSelectedBook(selectedBook));
+        },
+        setLoading(isLoading: boolean): void {
+          patchState(store, setLoading(isLoading));
+        },
+      };
+    })
+  );
+}
+
 export const BooksStore = signalStore(
   { providedIn: 'root' },
   withEntities<Book>(booksStoreConfig),
+  withCustomEntity(),
   withProps(() => ({
-    bookDataService: inject(BookDataService),
+    _bookDataService: inject(BookDataService),
   })),
-  withMethods(({ bookDataService, ...store }) => ({
+  withMethods(({ _bookDataService, ...store }) => ({
     listBooks: rxMethod<void>(
       pipe(
         debounceTime(3000),
         switchMap(() => {
-          return bookDataService.listBooks().pipe(
+          return _bookDataService.listBooks().pipe(
             tapResponse({
               next: (books) =>
                 patchState(store, setAllEntities(books, booksStoreConfig)),
